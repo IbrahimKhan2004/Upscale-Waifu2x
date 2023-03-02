aa58cec2d856072eb8dbff0ac1b573c249bbd031
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2023-03-02 09:53:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,32 +17,47 @@ bot = TelegramClient('image_upscale_bot', api_id, api_hash).start(bot_token=bot_
 @sleep_and_retry 
 @limits(calls=1, period=1) 
 def upscale_image_waifu2x(image_url): 
- # Upscale the image using Waifu2x 
- url = 'https://api.waifu2x.booru.pics/convert' 
- data = { 
-  'scale': 2, 
-  'noise': 0, 
-  'url': image_url, 
- } 
- response = requests.post(url, data=data) 
- if response.ok: 
-  image_data = BytesIO(response.content) 
-  image = Image.open(image_data) 
-  return image 
+    # Upscale the image using Waifu2x 
+    url = 'https://api.waifu2x.booru.pics/convert' 
+    data = { 
+        'scale': 2, 
+        'noise': 0, 
+        'url': image_url, 
+    } 
+    response = requests.post(url, data=data) 
+    try:
+        if response.ok: 
+            image_data = BytesIO(response.content) 
+            image = Image.open(image_data) 
+            return image 
+        else:
+            return None
+    except Exception as e:
+        print(f"Error upscaling image: {e}")
+        return None
+    finally:
+        image_data.close()
 
 # Define a message handler that will upscale the image sent by the user and send it back 
 @bot.on(events.NewMessage(pattern='/upscale')) 
 async def upscale_image(event): 
- # Download the image sent by the user 
- image_message = await event.get_reply_message() 
- if image_message and hasattr(image_message, 'photo'): 
-  # also checks if user has actually sent a photo 
-  image_media = image_message.photo 
-  image_url = image_media.sizes[-1].location # selects best quality image (last in array) 
-  # Upscale the image using Waifu2x 
-  image = upscale_image_waifu2x(image_url) 
-  # Send the upscaled image back to the user 
-  await bot.send_file(event.chat_id, image) 
+    # Download the image sent by the user 
+    image_message = await event.get_reply_message() 
+    if image_message and hasattr(image_message, 'photo'): 
+        # also checks if user has actually sent a photo 
+        image_media = image_message.photo 
+        image_url = image_media.sizes[-1].location # selects best quality image (last in array) 
+        # Upscale the image using Waifu2x 
+        image = upscale_image_waifu2x(image_url) 
+        if image:
+            # Send the upscaled image back to the user 
+            try:
+                output = BytesIO()
+                image.save(output, format='PNG')
+                output.seek(0)
+                await bot.send_file(event.chat_id, output, reply_to=image_message.id)
+            except Exception as e:
+                print(f"Error sending upscaled image: {e}")
 
 # Start the bot 
 bot.run_until_disconnected()
